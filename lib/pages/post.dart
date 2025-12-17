aa90b5fa10559967d83e5eacbf1835fec4f79be0
loltoulan <lol_toulan@163.com>
2025-12-17 20:59:00 +0800

    feat(post): 实现图片选择和展示功能

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
+import '../utils/constant.dart';
+
 class PostEditPage extends StatefulWidget {
   const PostEditPage({super.key});
 
@@ -9,20 +11,69 @@ class PostEditPage extends StatefulWidget {
 }
 
 class _PostEditPageState extends State<PostEditPage> {
-  _mainView() {
-    return Column(
-      children: [
-        ElevatedButton(
-          onPressed: () async {
-            final List<AssetEntity>? result = await AssetPicker.pickAssets(
-              context,
-            );
-            print(result?.length);
-          },
-          child: Text('选择图片'),
-        ),
-      ],
+  List<AssetEntity> photos = [];
+
+  Widget _buildPhotosList() {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final double width = (constraints.maxWidth - spacing * 2) / 3;
+        return Wrap(
+          spacing: spacing,
+          runSpacing: spacing,
+          children: [
+            for (final value in photos) photoViewContainer(value, width),
+
+            // 添加图片
+            if (photos.length < maxAssets)
+              gestureDetectorWithPhotoSelect(context, width),
+          ],
+        );
+      },
     );
+  }
+
+  GestureDetector gestureDetectorWithPhotoSelect(
+    BuildContext context,
+    double width,
+  ) {
+    return GestureDetector(
+      onTap: () async {
+        final List<AssetEntity>? result = await AssetPicker.pickAssets(
+          context,
+          pickerConfig: AssetPickerConfig(
+            selectedAssets: photos,
+            maxAssets: maxAssets,
+          ),
+        );
+        setState(() {
+          photos = result ?? [];
+        });
+      },
+      child: Container(
+        color: Colors.grey.shade200,
+        width: width,
+        height: width,
+        child: Icon(Icons.add, color: Colors.grey.shade500, size: 50),
+      ),
+    );
+  }
+
+  Container photoViewContainer(AssetEntity value, double width) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+      child: AssetEntityImage(
+        value,
+        key: ValueKey(value.id), // 添加key参数
+        fit: BoxFit.cover,
+        width: width,
+        height: width,
+      ),
+    );
+  }
+
+  Widget _mainView() {
+    return Column(children: [_buildPhotosList()]);
   }
 
   @override
---
+++ lib/utils/constant.dart
@@ -0,0 +1,2 @@
+ final double spacing = 10.0;
+ final int maxAssets = 9;
